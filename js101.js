a37a27f620778faea785ccca5c86006c518e4e8d
zihan zhao <zihanhannah@gmail.com>
2020-10-09 01:18:50 -0500

    exercise

--- js101.js
+++ js101.js
@@ -3,4 +3,10 @@
 // On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
 let onMarsRightNow = false;
 
+// Exercise 2
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+
+let fruits = ["mango", "banana", "guava", "kiwi", "strawberry"];
+
 
